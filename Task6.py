f3145feaf0941d00718ebaa5cc15ec9943cf58d0
MateuszParadowski <131271053+MateuszParadowski@users.noreply.github.com>
2023-06-06 20:53:59 +0200

    Update Task6.py

--- Task6.py
+++ Task6.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python
-from xml.etree.ElementTree import parse, Element, SubElement
+#wczytywanie do obiektu z pliku .xml i weryfikacja poprawności składni pliku
+import xmltodict
 
-def ReadBooks():
-    file = r"d:\pobrane\Narzędzia Pracy w Branży IT\Lab678\pathFile3.xml"
-    books = list()
-    doc = parse(file)
-    root = doc.getroot()
-    for bookElement in root.iterfind("book"):
-        title = bookElement.findtext("title")
-        book = Book(title)
-        book.author = bookElement.findtext("author")
-        book.publisher = bookElement.findtext("publisher")
-        book.publicationDate = bookElement.findtext("publication_date")
-        for chapterElement in bookElement.iterfind("chapters/chapter"):
-            title = chapterElement.findtext("title")
-            page = chapterElement.findtext("page")
-            chapter = Chapter(book, title, page)
-            chapter.number = chapterElement.get("number")
-            book.chapters.append(chapter)
-        book.chapters.sort(key = lambda c : int(c.page))
-        books.append(book)
-    return books
+def open_xml('pathFile3.xml'):
+    try:
+        with open('pathFile3.xml', 'r') as plik:
+            a = xmltodict.parse(plik.read())
+            return a
+    except FileNotFoundError:
+        print("Taki plik nie istnieje")
+    else:
+        print("Taki plik istnieje")
