477b5afd827c47c5a2658870bf1e4d0813a41f52
MateuszParadowski <131271053+MateuszParadowski@users.noreply.github.com>
2023-06-06 21:24:44 +0200

    Update Task7.py

--- Task7.py
+++ Task7.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python
 #zapis danych z obiektu do pliku w formacie i zgodnie ze składnią .xml
+import sys
 import xmltodict
-def save_xml(a, 'pathFile3.xml'):
+if len(sys.argv) != 2:
+        print("Dozwolony jest tylko jeden argument")
+        return
+    else:
+        pass
+save_file = sys.argv
+a = None
+def save_xml(a, save_file):
     try:
         with open(a, 'w') as plik:
-            xml_str = xmltodict.unparse('pathFile3.xml', pretty=True)
+            xml_str = xmltodict.unparse(save_file, pretty=True)
             plik.write(xml_str)
         print("Udana konwersja do formatu XML")
     except Exception:
