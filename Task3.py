6137723ca36821886baad20b0b99dd49f67e5980
MateuszParadowski <131271053+MateuszParadowski@users.noreply.github.com>
2023-06-06 20:24:12 +0200

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,4 +1,10 @@
+#zapis danych z obiektu do pliku w formacie i zgodnie ze składnią .json
 #!/usr/bin/python
 import json
-with open('new_file.json','w') as file:
-  json.dump(py_dict,file)
+def save_json(a, save_file):
+    try:
+        with open(save_file, 'w') as plik:
+            json.dump(a, plik)
+        print("Udana konwersja do formatu JSON")
+    except Exception:
+        print("Błąd")
