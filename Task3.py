66ce5c5ff673abb8a5046942f8f1ca1fdbbc7a0c
MateuszParadowski <131271053+MateuszParadowski@users.noreply.github.com>
2023-06-06 21:23:01 +0200

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python
 #zapis danych z obiektu do pliku w formacie i zgodnie ze składnią .json
+import sys
 import json
-def save_json(a, 'pathFile1.json'):
+if len(sys.argv) != 2:
+        print("Dozwolony jest tylko jeden argument")
+        return
+    else:
+        pass
+save_file = sys.argv
+a = None
+def save_json(a, save_file):
     try:
-        with open('pathFile1.json', 'w') as plik:
+        with open(save_file, 'w') as plik:
             json.dump(a, plik)
         print("Udana konwersja do formatu JSON")
     except Exception:
