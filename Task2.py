059a082e387b135478b1c8cc9ad668a4fe25edbf
MateuszParadowski <131271053+MateuszParadowski@users.noreply.github.com>
2023-06-06 21:18:30 +0200

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 #wczytywanie do obiektu z pliku .json i weryfikacja poprawności składni pliku
+import sys
 import json
-def open_json('pathFile1.json'):
+if len(sys.argv) != 2:
+        print("Dozwolony jest tylko jeden argument")
+        return
+    else:
+        pass
+open_file = sys.argv
+def open_json(open_file):
     try:
-        with open('pathFile1.json', 'r') as plik:
+        with open(open_file, 'r') as plik:
             a = json.load(plik)
             return a
     except json.JSONDecodeError:
