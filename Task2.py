0aa8478efbef317a04bce4afc5b69102690b0a88
MateuszParadowski <131271053+MateuszParadowski@users.noreply.github.com>
2023-06-06 20:28:29 +0200

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,12 +1,14 @@
 #wczytywanie do obiektu z pliku .json i weryfikacja poprawności składni pliku
 #!/usr/bin/python
 import json
-def podaj_plik_json(plik_wej):
+def open_json(open_file):
     try:
-        with open(plik_wej, 'r') as plik:
+        with open(open_file, 'r') as plik:
             a = json.load(plik)
             return a
     except json.JSONDecodeError:
         print("Taki plik nie istnieje, sprawdź nazwę")
     except FileNotFoundError:
         print("Taki plik nie istnieje")
+    else:
+        print("Taki plik istnieje")
