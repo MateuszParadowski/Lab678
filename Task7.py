db8a22b9ad4da5a6ef7d631ebb0723c820dc9484
MateuszParadowski <131271053+MateuszParadowski@users.noreply.github.com>
2023-06-06 20:59:33 +0200

    Update Task7.py

--- Task7.py
+++ Task7.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python
-import xml.etree.ElementTree as et
-d = et.parse("pathFile3.xml")
-d.write("pathFile3.1.xml",encoding="utf-8")
+#zapis danych z obiektu do pliku w formacie i zgodnie ze składnią .xml
+import xmltodict
+def save_xml(a, 'pathFile3.xml'):
+    try:
+        with open(a, 'w') as plik:
+            xml_str = xmltodict.unparse('pathFile3.xml', pretty=True)
+            plik.write(xml_str)
+        print("Udana konwersja do formatu XML")
+    except Exception:
+        print("Błąd")
