7527f7a1fb95cea2dbb17400291272f2ea5d2f69
MateuszParadowski <131271053+MateuszParadowski@users.noreply.github.com>
2023-06-07 09:42:22 +0200

    Update Task5.py

--- Task5.py
+++ Task5.py
@@ -2,11 +2,12 @@
 #zapis danych z obiektu do pliku w formacie i zgodnie ze składnią .yaml
 import sys
 import yaml
-if len(sys.argv) != 2:
-        print("Dozwolony jest tylko jeden argument")
-        return
-    else:
-        pass
+def main():
+        if len(sys.argv) != 2:
+                print("Dozwolony jest tylko jeden argument")
+                return
+        else:
+                pass
 save_file = sys.argv
 a = None
 def save_yaml(a, save_file):
