18ef8cf267927fae5eba782b6809f75c246211ad
MateuszParadowski <131271053+MateuszParadowski@users.noreply.github.com>
2023-06-07 09:24:31 +0200

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -4,30 +4,30 @@ import sys
 if len(sys.argv) != 3:
         print("Dozwolone sa tylko dwa argumenty")
         return
-    else:
+else:
         pass
 
-    open_file = sys.argv[1]
-    save_file = sys.argv[2]
+open_file = sys.argv[1]
+save_file = sys.argv[2]
 
-    a = None
+a = None
 
-    if open_file.endswith('.json'):
+if open_file.endswith('.json'):
         a = open_json(open_file)
-    elif open_file.endswith('.yaml'):
+elif open_file.endswith('.yaml'):
         a = open_yaml(open_file)
-    elif plik_wej.endswith('.xml'):
+elif plik_wej.endswith('.xml'):
         a = open_xml(open_file)
-    else:
+else:
         print("Błędny format pliku.")
         return
 
-    if save_file.endswith('.json'):
+if save_file.endswith('.json'):
         save_json(a, save_file)
-    elif plik_wyj.endswith('.yaml'):
+elif plik_wyj.endswith('.yaml'):
         save_yaml(a, save_file)
-    elif plik_wyj.endswith('.xml'):
+elif plik_wyj.endswith('.xml'):
         save_xml(a, save_file)
-    else:
+else:
         print("Błędny format pliku.")
         return
